2c87420d5750a44e7dc231d353c0b159ee4d885b
hupengfei <gpstudent@EGGiMac11.local>
2017-03-08 16:19:30 -0800

    shooting part
    however, there are some bugs.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,16 +9,24 @@ public class Player : MonoBehaviour {
 	
 	Weapon weapon;
 	WeaponController weaponController;
+//	Camera viewCamera;
 
 	void Start() {
 		weaponController = GetComponent<WeaponController> ();
 	}
 
-
 	void Update() {
-		
+		float rayDistance;
+//		Ray ray = viewCamera.ScreenPointToRay (Input.mousePosition);
+		Plane groundPlane = new Plane (Vector3.up, Vector3.zero);
 		if (Input.GetMouseButton (0)) {
-			weapon.Shoot ();
+//			weapon.Shoot ();
+//			LookAt(ray.GetPoint(out rayDistance));
+			weaponController.Shoot();
 		}
+	}
+
+	public void LookAt(Vector3 lookPoint) {
+		transform.LookAt (lookPoint);
 	}
 }
--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -4,7 +4,24 @@ using UnityEngine;
 
 public class WeaponController : MonoBehaviour {
 
+
 	public Weapon weapon;
+	public Transform weaponHold;
+
+	void Start() {
+		if (weapon != null) {
+			EquipWeapon (weapon);
+		}
+	}
+
+	public void EquipWeapon(Weapon weaponToEquip) {
+		if (weapon != null) {
+			Destroy (weapon.gameObject);
+		}
+		weapon = Instantiate (weaponToEquip, weaponHold.position, weaponHold.rotation ) as Weapon;
+		weapon.transform.parent = weaponHold;
+	}
+
 	public void Shoot() {
 		if (weapon != null) {
 			weapon.Shoot ();
